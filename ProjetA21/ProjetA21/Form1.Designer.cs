f834840780e9b61941a946d5901ce36088985878
gille <gille@DESKTOP-60LBSLJ>
2017-05-19 16:13:05 +0200

    zssdfghj

--- ProjetA21/ProjetA21/Form1.Designer.cs
+++ ProjetA21/ProjetA21/Form1.Designer.cs
@@ -28,17 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(231, 73);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(186, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Date dépense";
             // 
             // frmBdgMois
             // 
             this.ClientSize = new System.Drawing.Size(977, 540);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
             this.Name = "frmBdgMois";
+            this.Load += new System.EventHandler(this.frmBdgMois_Load_1);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- ProjetA21/ProjetA21/Form1.cs
+++ ProjetA21/ProjetA21/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace ProjetA21
 {
@@ -17,11 +18,17 @@ namespace ProjetA21
             InitializeComponent();
         }
 
-        private void frmBdgMois_Load(object sender, EventArgs e)
+     
+
+        private void frmBdgMois_Load_1(object sender, EventArgs e)
         {
-            string title = "TabPage " + (tabControl1.TabCount + 1).ToString();
-            TabPage myTabPage = new TabPage(title);
-            tabControl1.TabPages.Add(myTabPage);
+            DateTime date1 = new DateTime(2010, 3, 14, 2, 30, 0, DateTimeKind.Local);
+            Console.WriteLine("Invalid time: {0}",
+                              TimeZoneInfo.Local.IsInvalidTime(date1));
+            DateTime utcDate1 = date1.ToUniversalTime();
+            DateTime date2 = utcDate1.ToLocalTime();
+
+            MessageBox.Show(date2.ToString());
 
         }
     }
