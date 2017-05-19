06db339b93d66c467f9f1b7b828314d04ca19afa
Jean-Noël Balanche <jean-noel.balanche@etu.unistra.fr>
2017-05-19 15:07:50 +0200

    test bite

--- BudGestion/BudGestion/Form1.cs
+++ BudGestion/BudGestion/Form1.cs
@@ -16,5 +16,10 @@ namespace BudGestion
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("bite");
+        }
     }
 }
